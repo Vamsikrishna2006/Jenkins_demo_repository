38a28a3f464cf4e6c80cb3dfb17e538c40d3440e
Vamsikrishna2006 <vamsiroy.2006@gmail.com>
2026-02-14 14:53:59 +0530

    Welcome to Jenkins Message

--- src/main/java/jenkins_demo/jenkins_demo1/App.java
+++ src/main/java/jenkins_demo/jenkins_demo1/App.java
@@ -5,6 +5,6 @@ package jenkins_demo.jenkins_demo1;
  */
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Welcome to Jenkins");
     }
 }
