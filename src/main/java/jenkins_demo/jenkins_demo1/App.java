578222049ec272a8b684b869c0cff13ae8c0460d
Vamsikrishna2006 <vamsiroy.2006@gmail.com>
2026-02-14 14:40:01 +0530

    initial commit

---
+++ src/main/java/jenkins_demo/jenkins_demo1/App.java
@@ -0,0 +1,10 @@
+package jenkins_demo.jenkins_demo1;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
